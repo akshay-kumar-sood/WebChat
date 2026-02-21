2b354f08db27a26893dc12c2d45e3dfc667bd81b
AKSHAY KUMAR SOOD <id.akshay23@gmail.com>
2026-02-21 22:18:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import { createServer } from 'node:http';
 import express from 'express';
 import { Server } from 'socket.io';
-
+const PORT = process.env.PORT;
 const app = express();
 
 const server = createServer(app);
@@ -47,6 +47,6 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
 
-server.listen(4600, () => {
+server.listen(PORT, () => {
     console.log('server running at http://localhost:4600');
 });
