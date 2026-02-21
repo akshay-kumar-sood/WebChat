fd1646fa43eb99256d9c4218ff2f850c584707fb
AKSHAY KUMAR SOOD <id.akshay23@gmail.com>
2026-02-21 22:23:37 +0530

    Update ws.js

--- frontend/src/ws.js
+++ frontend/src/ws.js
@@ -1,6 +1,10 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
 export function connectWS() {
-    const socketUrl = import.meta.env.VITE_SOCKET_URL || 'http://localhost:4600';
-    return io(socketUrl);
+    const socketUrl =
+        import.meta.env.VITE_SOCKET_URL || window.location.origin;
+
+    return io(socketUrl, {
+        transports: ["websocket"],
+    });
 }
